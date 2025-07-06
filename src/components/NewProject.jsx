bdc3eed5ca741ee2d778af84c804d175d9fdf8a4
Francesco De Vivo <francescodvf@yahoo.it>
2025-07-06 12:53:17 +0200

    Set State for NewProjects.jsx

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export default function NewProject() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDescription, setEnteredDescription] = useState("");
+  const [enteredDueDate, setEnteredDueDate] = useState("");
+
   return (
     <form className="mt-4 text-right" action="">
       <div className="mb-4">
@@ -8,7 +14,12 @@ export default function NewProject() {
         >
           Title
         </label>
-        <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" type="text" />
+        <input
+          className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+          type="text"
+          value={enteredTitle}
+          onChange={(event) => setEnteredTitle(event.target.value)}
+        />
       </div>
       <div className="mb-4">
         <label
@@ -20,6 +31,8 @@ export default function NewProject() {
         <textarea
           className="w-64 px-2 py-1 rounded-sm bg-stone-200"
           rows="5"
+          value={enteredDescription}
+          onChange={(event) => setEnteredDescription(event.target.value)}
         ></textarea>
       </div>
       <div className="mb-4">
@@ -29,18 +42,25 @@ export default function NewProject() {
         >
           Due Date
         </label>
-        <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" type="date" />
+        <input
+          className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+          type="date"
+          value={enteredDueDate}
+          onChange={(event) => setEnteredDueDate(event.target.value)}
+        />
       </div>
       <div className="flex justify-end space-x-2">
         <button
           type="button"
           className="px-4 py-2 bg-stone-300 rounded hover:bg-stone-400"
+          // onCancel={} // define onCancel function
         >
           Cancel
         </button>
         <button
           type="submit"
           className="px-4 py-2 bg-stone-800 text-white rounded hover:bg-stone-700"
+          onSubmit={hadleSubmit} // define hadleSubmit function
         >
           Save
         </button>
